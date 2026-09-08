60d3cebd773d43c35298ab7af1afd17bac4af333
anilkummar <uanilchoudary98@gmail.com>
2026-09-08 22:32:28 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -11,7 +11,20 @@ if [ $USER -ne 0 ]; then
     echo "User doesnot have permission to run this script" 
     exit 1
 fi
+
+VALIDATE()
+{
+    if [ $1 -eq 0 ]; then
+        echo "$2 .... SUCCESS"
+    else 
+        echo "$2 ... FAILURE"
+        
+    fi
+
+}
+
 mkdir -p $LOG_DIR
+VALIDATE $? "Log dir creation"
 
 for INSTANCE in $@
 do
@@ -22,6 +35,7 @@ do
         --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value='$INSTANCE'}]" \
         --query 'Instances[0].InstanceId' \
         --output text) &>> $LOG_FILE
+    VALIDATE $? "$INSTANCE Creation is"
 
     echo "created $INSTANCE" | tee -a $LOG_FILE
 
@@ -31,6 +45,8 @@ do
         --instance-ids $INSTANCE_ID \
         --query 'Reservations.Instances.PublicIpAddress' \
         --output text) &>> $LOG_FILE
+
+        VALIDATE $? "IP Creation is"
         echo "Public IP address is $IP"  | tee -a $LOG_FILE
     
     else
@@ -38,6 +54,7 @@ do
         --instance-ids $INSTANCE_ID \
         --query 'Reservations.Instances.PrivateIpAddress' \
         --output text) &>> $LOG_FILE
+        VALIDATE $? "IP Creation is"
         echo "Private IP address is $IP"  | tee -a $LOG_FILE
     
     fi
