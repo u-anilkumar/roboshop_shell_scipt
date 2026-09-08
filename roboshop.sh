d23a0b9a944e4f188420126bd86987444c986987
anilkummar <uanilchoudary98@gmail.com>
2026-09-08 22:22:20 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -4,11 +4,15 @@ USER=$(id -u)
 AMI_ID="ami-0220d79f3f480ecf5"
 SG_ID="sg-03dde207ea3271219"
 INS_TYPE="t3.micro"
+LOG_DIR="/var/log/roboshop/"
+LOG_FILE=$LOG_DIR/$0.log
 
 if [ $USER -ne 0 ]; then
-    echo "User doesnot have permission to run this script"
+    echo "User doesnot have permission to run this script" 
     exit 1
 fi
+mkdir -p $LOG_DIR
+
 for INSTANCE in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances \
@@ -17,23 +21,24 @@ do
         --security-group-ids $SG_ID \
         --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value='$INSTANCE'}]" \
         --query 'Instances[0].InstanceId' \
-        --output text)
-    echo "created $INSTANCE"
-    
+        --output text) &>> $LOG_FILE
+
+    echo "created $INSTANCE" | tee -a $LOG_FILE
+
     if [ $INSTANCE == "frontend" ]; then
 
         IP=$(aws ec2 describe-instances \
         --instance-ids $INSTANCE_ID \
         --query 'Reservations.Instances.PublicIpAddress' \
-        --output text)
-        echo "Public IP address is $IP"
+        --output text) &>> $LOG_FILE
+        echo "Public IP address is $IP"  | tee -a $LOG_FILE
     
     else
         IP=$(aws ec2 describe-instances \
         --instance-ids $INSTANCE_ID \
         --query 'Reservations.Instances.PrivateIpAddress' \
-        --output text)
-        echo "Private IP address is $IP"
+        --output text) &>> $LOG_FILE
+        echo "Private IP address is $IP"  | tee -a $LOG_FILE
     
     fi
     
