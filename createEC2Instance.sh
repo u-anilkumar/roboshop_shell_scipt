ce6347a762e94734e03019250a39a6ddeccb63ab
anilkummar <uanilchoudary98@gmail.com>
2026-09-09 10:32:53 +0530

    Create EC2Instance

--- createEC2Instance.sh
+++ createEC2Instance.sh
@@ -13,23 +13,64 @@ VALIDATE(){
 AMI_ID="ami-0220d79f3f480ecf5"
 SG_ID="sg-03dde207ea3271219"
 INS_TYPE="t3.micro"
+HOST_ZONE_ID="Z0124704QJP234EU1AXQ"
+DOMAIN="anildevops.online"
 
+for INSTANCE in $@
+do
 INSTANCE_ID=$(aws ec2 run-instances \
     --image-id $AMI_ID \
     --instance-type $INS_TYPE \
     --security-group-ids $SG_ID \
-    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value='$1'}]' \
+    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value='$INSTANCE'}]' \
     --query 'Instances[0].InstanceId' \
     --output text) 
 
-VALIDATE $? "INSTANCE Creation with name $1 "    
-echo "Instance name is $1 and Instance id is $INSTANCE_ID"
+VALIDATE $? "INSTANCE Creation with name $INSTANCE "    
+echo "Instance name is $INSTANCE and Instance id is $INSTANCE_ID"
 
-IP=$(aws ec2 describe-instances \
+
+
+if [ $INSTANCE == frontend ]; then
+    IP=$(aws ec2 describe-instances \
+        --instance-ids $INSTANCE_ID \
+        --query 'Reservations[0].Instances[0].PublicIpAddress' \
+        --output text)
+    VALIDATE $? "Public IP Creation "  
+    DNS_NAME=$DOMAIN
+    echo "Public IP is $IP"
+else
+    IP=$(aws ec2 describe-instances \
     --instance-ids $INSTANCE_ID \
     --query 'Reservations[0].Instances[0].PrivateIpAddress' \
     --output text)
-VALIDATE $? "IP Creation "  
-echo "Private IP is $IP"
+    VALIDATE $? "IP Creation "  
+    DNS_NAME=$INSTANCE.$DOMAIN
+    echo "Private IP is $IP"
+fi
+
+aws route53 change-resource-record-sets \
+    --hosted-zone-id $HOST_ZONE_ID \
+    --change-batch '{
+        "Comment": "Updating A record to new IP",
+        "Changes": [
+            {
+                "Action": "UPSERT",
+                "ResourceRecordSet": {
+                    "Name": "$DNS_NAME",
+                    "Type": "A",
+                    "TTL": 1,
+                    "ResourceRecords": [
+                        {
+                            "Value": "$IP"
+                        }
+                    ]
+                }
+            }
+        ]
+    }'
+
+done
+
 
 
